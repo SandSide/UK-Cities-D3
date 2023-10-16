a80aef029561d565f9d68e30b2d7ff27e981fc1b
SandSide <37347875+SandSide@users.noreply.github.com>
2023-10-16 23:58:46 +0100

    chore: remove old code

--- src/script.js
+++ src/script.js
@@ -1,54 +1,3 @@
-// function drawMap() {
-//     const width = 800;
-//     const height = 600;
-
-//     var svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
-
-//     const path = d3.geoPath(projection);
-
-//     const g = svg.append('g');
-
-//     d3.json('https://martinjc.github.io/UK-GeoJSON/json/eng/topo_eer.json')
-//         .then(data => {
-
-//             const countries = topojson.feature(data, data.objects.eer);
-
-//             g.selectAll('path').data(countries.features).enter().append('path').attr('class', 'country').attr('d', path)
-//                 .style("fill", d => {
-
-//                     if (d.properties.EER13NM == "North East") return "green";
-//                     else return "red";
-//                 })
-//         }).catch(err => {
-//             throw err;
-//         });
-// }
-
-// function drawMap3() {
-
-//     const width = 2000;
-//     const height = 6000;
-
-//     let projection = d3.geoNaturalEarth1();
-//     let path = d3.geoPath().projection(projection);
-//     //let svg = d3.select("#worldMap").append('svg').append("g");
-//     var svg = d3.select('body').append('svg').attr('width', width).attr('height', height).append("g");
-
-//     d3.json('https://martinjc.github.io/UK-GeoJSON/json/eng/topo_eer.json')
-//         .then(data => {
-//             const uk = topojson.feature(data, data.objects.eer);
-
-//             svg.selectAll('path')
-//                 .data(uk.features)
-//                 .enter()
-//                 .append('path')
-//                 .attr("stroke","#313131")
-//                 .attr("stroke-width",1)
-//                 .attr("fill", "#535353")
-//                 .attr("d", path);
-//         });
-// }
-
 function drawMap4() {
     const width = 700;
     const height = 900;
@@ -104,7 +53,7 @@ function placePoints() {
         });
 }
 
-function test() {
+function displayMap() {
 
     var map = d3.json("assets/united-kingdom-detailed-boundary_1061.geojson");
     var cities = d3.json("http://34.38.72.236/Circles/Towns/50");
@@ -143,7 +92,7 @@ function drawMap(map, cities) {
         .attr("class", "country")
         .attr("d", path);
 
-    var cities = svg.selectAll('.point')
+    var c = svg.selectAll('.point')
         .data(cities)
         .enter()
         .append('circle')
@@ -152,22 +101,18 @@ function drawMap(map, cities) {
         .attr("r", 5)
         .attr("fill", "red");
 
-    // svg.selectAll(".point-label")
-    //     .data(data)
-    //     .enter()
-    //     .append("text")
-    //     .attr("class", "point-label")
-    //     .attr("x", function(d) { return projection([d.lng, d.lat])[0] + 10; })
-    //     .attr("y", function(d) { return projection([d.lng, d.lat])[1]; })
-    //     .text(function(d) { return d.Town; });
-
-
+    svg.selectAll(".point-label")
+        .data(cities)
+        .enter()
+        .append("text")
+        .attr("class", "point-label")
+        .attr("x", function(d) { return projection([d.lng, d.lat])[0] + 10; })
+        .attr("y", function(d) { return projection([d.lng, d.lat])[1]; })
+        .text(function(d) { return d.Town; });
 }
 
 
 window.onload = function () {
-    test();
-    // drawMap4();
-    // placePoints();
+    displayMap();
 };
 
