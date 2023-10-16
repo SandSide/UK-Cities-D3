5c04105dd920fa0d73da1e1353b42fd45cf5764c
SandSide <37347875+SandSide@users.noreply.github.com>
2023-10-16 23:56:03 +0100

    refactor: separate file fetch and map draw

--- src/script.js
+++ src/script.js
@@ -49,17 +49,17 @@
 //         });
 // }
 
-function drawMap4(){
+function drawMap4() {
     const width = 700;
     const height = 900;
 
     var svg = d3.select('body')
-                .append('svg')
-                .attr('width', width)
-                .attr('height', height)
-                .attr('id', "map")
-                .style('margin', '0 auto');
-        
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('id', "map")
+        .style('margin', '0 auto');
+
     var g = svg.append('g');
 
     d3.json("assets/united-kingdom-detailed-boundary_1061.geojson")
@@ -67,9 +67,9 @@ function drawMap4(){
             const uk = data; topojson.feature(data, data.features);
 
             let projection = d3.geoEquirectangular()
-            .center([-1.3,58])
-      
-            .scale(2000)
+                .center([-1.3, 58])
+
+                .scale(2000)
 
             let path = d3.geoPath().projection(projection);
 
@@ -85,9 +85,9 @@ function drawMap4(){
         });
 }
 
-function placePoints(){
+function placePoints() {
 
-	var svg = d3.select("#map");
+    var svg = d3.select("#map");
 
     d3.json("http://34.38.72.236/Circles/Towns/10")
         .then(data => {
@@ -104,71 +104,68 @@ function placePoints(){
         });
 }
 
-function test(){
+function test() {
+
+    var map = d3.json("assets/united-kingdom-detailed-boundary_1061.geojson");
+    var cities = d3.json("http://34.38.72.236/Circles/Towns/50");
+
+    Promise.all([map, cities])
+        .then(data => {
+            drawMap(data[0], data[1]);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+function drawMap(map, cities) {
     const width = 600;
     const height = 900;
 
-    // var map = d3.json("assets/united-kingdom-detailed-boundary_1061.geojson");
-    // var cities =   d3.json("http://34.38.72.236/Circles/Towns/10");
-
     let projection = d3.geoMercator()
-    .center([1,58])
-    .scale(2500);
+        .center([1, 58])
+        .scale(2500);
 
     var svg = d3.select('body')
-                .append('svg')
-                .attr('width', width)
-                .attr('height', height)
-                .attr('id', "map");
-        
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('id', "map");
+
     var g = svg.append('g');
 
-    d3.json("assets/united-kingdom-detailed-boundary_1061.geojson")
-        .then(data => {
-            const uk = data;
+    let path = d3.geoPath().projection(projection);
 
-            let path = d3.geoPath().projection(projection);
+    g.selectAll('path')
+        .data(map.features)
+        .enter()
+        .append('path')
+        .attr("class", "country")
+        .attr("d", path);
 
-            g.selectAll('path')
-                .data(uk.features)
-                .enter()
-                .append('path')
-                .attr("class", "country")
-                .attr("d", path);
+    var cities = svg.selectAll('.point')
+        .data(cities)
+        .enter()
+        .append('circle')
+        .attr("cx", function (d) { return projection([d.lng, d.lat])[0]; })
+        .attr("cy", function (d) { return projection([d.lng, d.lat])[1]; })
+        .attr("r", 5)
+        .attr("fill", "red");
 
-        }).catch(err => {
-            console.error(err);
-        });
+    // svg.selectAll(".point-label")
+    //     .data(data)
+    //     .enter()
+    //     .append("text")
+    //     .attr("class", "point-label")
+    //     .attr("x", function(d) { return projection([d.lng, d.lat])[0] + 10; })
+    //     .attr("y", function(d) { return projection([d.lng, d.lat])[1]; })
+    //     .text(function(d) { return d.Town; });
 
 
-    d3.json("http://34.38.72.236/Circles/Towns/10")
-    .then(data => {
-
-        var cities = svg.selectAll('.point')
-            .data(data)
-            .enter()
-            .append('circle')
-            .attr("cx", function(d) { return projection([d.lng, d.lat])[0]; })
-            .attr("cy", function(d) { return projection([d.lng, d.lat])[1]; })
-            .attr("r", 5)
-            .attr("fill", "red");
-
-            // svg.selectAll(".point-label")
-            //     .data(data)
-            //     .enter()
-            //     .append("text")
-            //     .attr("class", "point-label")
-            //     .attr("x", function(d) { return projection([d.lng, d.lat])[0] + 10; })
-            //     .attr("y", function(d) { return projection([d.lng, d.lat])[1]; })
-            //     .text(function(d) { return d.Town; });
-
-    }).catch(err => {
-        console.error(err);
-    });
 }
 
 
-window.onload = function(){
+window.onload = function () {
     test();
     // drawMap4();
     // placePoints();
