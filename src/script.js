7db4ca35249cf95ce2ea4671b96d2f8c76d1de79
SandSide <37347875+SandSide@users.noreply.github.com>
2023-10-16 23:13:35 +0100

    feat: fetch cities data

--- src/script.js
+++ src/script.js
@@ -1,74 +1,74 @@
-function drawMap() {
-    const width = 800;
-    const height = 600;
+// function drawMap() {
+//     const width = 800;
+//     const height = 600;
 
-    var svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
+//     var svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
 
-    const path = d3.geoPath(projection);
+//     const path = d3.geoPath(projection);
 
-    const g = svg.append('g');
+//     const g = svg.append('g');
 
-    d3.json('https://martinjc.github.io/UK-GeoJSON/json/eng/topo_eer.json')
-        .then(data => {
+//     d3.json('https://martinjc.github.io/UK-GeoJSON/json/eng/topo_eer.json')
+//         .then(data => {
 
-            const countries = topojson.feature(data, data.objects.eer);
+//             const countries = topojson.feature(data, data.objects.eer);
 
-            g.selectAll('path').data(countries.features).enter().append('path').attr('class', 'country').attr('d', path)
-                .style("fill", d => {
+//             g.selectAll('path').data(countries.features).enter().append('path').attr('class', 'country').attr('d', path)
+//                 .style("fill", d => {
 
-                    if (d.properties.EER13NM == "North East") return "green";
-                    else return "red";
-                })
-        }).catch(err => {
-            throw err;
-        });
-}
+//                     if (d.properties.EER13NM == "North East") return "green";
+//                     else return "red";
+//                 })
+//         }).catch(err => {
+//             throw err;
+//         });
+// }
 
-function drawMap3() {
+// function drawMap3() {
 
-    const width = 2000;
-    const height = 6000;
+//     const width = 2000;
+//     const height = 6000;
 
-    let projection = d3.geoNaturalEarth1();
-    let path = d3.geoPath().projection(projection);
-    //let svg = d3.select("#worldMap").append('svg').append("g");
-    var svg = d3.select('body').append('svg').attr('width', width).attr('height', height).append("g");
+//     let projection = d3.geoNaturalEarth1();
+//     let path = d3.geoPath().projection(projection);
+//     //let svg = d3.select("#worldMap").append('svg').append("g");
+//     var svg = d3.select('body').append('svg').attr('width', width).attr('height', height).append("g");
 
-    d3.json('https://martinjc.github.io/UK-GeoJSON/json/eng/topo_eer.json')
-        .then(data => {
-            const uk = topojson.feature(data, data.objects.eer);
+//     d3.json('https://martinjc.github.io/UK-GeoJSON/json/eng/topo_eer.json')
+//         .then(data => {
+//             const uk = topojson.feature(data, data.objects.eer);
 
-            svg.selectAll('path')
-                .data(uk.features)
-                .enter()
-                .append('path')
-                .attr("stroke","#313131")
-                .attr("stroke-width",1)
-                .attr("fill", "#535353")
-                .attr("d", path);
-        });
-}
+//             svg.selectAll('path')
+//                 .data(uk.features)
+//                 .enter()
+//                 .append('path')
+//                 .attr("stroke","#313131")
+//                 .attr("stroke-width",1)
+//                 .attr("fill", "#535353")
+//                 .attr("d", path);
+//         });
+// }
 
 function drawMap4(){
-    const width = 900;
+    const width = 700;
     const height = 900;
 
     var svg = d3.select('body')
                 .append('svg')
                 .attr('width', width)
                 .attr('height', height)
+                .attr('id', "map")
                 .style('margin', '0 auto');
         
     var g = svg.append('g');
 
     d3.json("assets/united-kingdom-detailed-boundary_1061.geojson")
         .then(data => {
-
             const uk = data; topojson.feature(data, data.features);
 
             let projection = d3.geoEquirectangular()
+            .center([-3,58])
             .scale(4000)
-            .fitSize([width, height], uk);
 
             let path = d3.geoPath().projection(projection);
 
@@ -85,4 +85,26 @@ function drawMap4(){
         });
 }
 
-window.onload = drawMap4;
+function placePoints(){
+
+	Width = 300;
+	Height = 200;
+
+	var svg = d3.select("#map");
+
+    d3.json("http://34.38.72.236/Circles/Towns/50")
+        .then(data => {
+
+            var cities = svg.selectAll('circle')
+                .data(data)
+                .enter()
+                .append('circle')
+                .attr("cx", 10)
+                .attr("cy", 10)
+                .attr("r", 5);
+        }).catch(err => {
+            console.error(err);
+        });
+}
+
+window.onload = placePoints;
