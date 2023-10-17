a39179374b1544d6795a67eeb90dd8b339b253da
SandSide <37347875+SandSide@users.noreply.github.com>
2023-10-17 13:45:05 +0100

    chore: remove unused code

--- src/script.js
+++ src/script.js
@@ -4,27 +4,26 @@ let projection;
 function displayMap() {
 
     var map = d3.json("assets/united-kingdom-detailed-boundary_1061.geojson");
-    var cities = d3.json("http://34.38.72.236/Circles/Towns/25");
 
-    Promise.all([map, cities])
+    Promise.all([map])
         .then(data => {
-            drawMap(data[0], data[1]);
+            drawMap(data[0]);
         })
         .catch(err => {
             console.error(err);
         });
 }
 
-function drawMap(map, cities) {
+function drawMap(map) {
     const width = 600;
     const height = 900;
 
-    // var amountToDispaly = 
-
+    // Set projection
     projection = d3.geoMercator()
         .center([1, 58])
         .scale(2500);
-
+    
+    // Create map container
     var svg = d3.select('.map-container')
         .append('svg')
         .attr('width', width)
@@ -35,6 +34,7 @@ function drawMap(map, cities) {
 
     let path = d3.geoPath().projection(projection);
 
+    // Draw map
     g.selectAll('path')
         .data(map.features)
         .enter()
@@ -42,36 +42,11 @@ function drawMap(map, cities) {
         .attr("class", "country")
         .attr('d', path);
 
-    // var c = svg.selectAll('.point')
-    //     .data(cities)
-    //     .enter()
-    //     .append('circle')
-    //     .attr('cx', function (d) { return projection([d.lng, d.lat])[0]; })
-    //     .attr('cy', function (d) { return projection([d.lng, d.lat])[1]; })
-    //     .attr('r', d => d.Population/12000)
-    //     .attr('class', 'point')
-    //     .on('mouseover', function(){
-    //         d3.select(this).classed('hover', true);
-    //     })
-    //     .on("mouseout", function(){
-    //         d3.select(this).classed('hover', false);
-    //     })
-
-    // svg.selectAll('.point-label')
-    //     .data(cities)
-    //     .enter()
-    //     .append('text')
-    //     .attr('class', 'point-label')
-    //     .attr('x', function(d) { return projection([d.lng, d.lat])[0] + 10})
-    //     .attr('y', function(d) { return projection([d.lng, d.lat])[1] })
-    //     .text(function(d) { return d.Town; })
-    //     .on("mouseover", function(d){
-
-    //     });
 }
 
 function displayTowns() {
 
+    // Get number to display
     var numToDisplay = document.getElementById('townRange').value;
 
     var temp = d3.json("http://34.38.72.236/Circles/Towns/" + numToDisplay)
@@ -83,6 +58,7 @@ function displayTowns() {
             svg.selectAll("circle.point").remove();
             svg.selectAll("text.point-label").remove();
 
+            // Plot points
             var c = svg.selectAll('.point')
                 .data(data)
                 .enter()
@@ -98,6 +74,7 @@ function displayTowns() {
                     d3.select(this).classed('hover', false);
                 })
 
+            // Plot points town name
             svg.selectAll('.point-label')
                 .data(data)
                 .enter()
