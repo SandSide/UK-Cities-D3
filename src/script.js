5f2434e46337d26311453e41dbb2114deae117da
SandSide <37347875+SandSide@users.noreply.github.com>
2023-10-18 01:46:49 +0100

    refactor: convert coord calculation into a function

--- src/script.js
+++ src/script.js
@@ -16,11 +16,12 @@ function displayMap() {
 function drawMap(map) {
     const width = 600;
     const height = 900;
+    const scale = 2500;
 
     // Set projection
     projection = d3.geoMercator()
         .center([1, 58])
-        .scale(2500);
+        .scale(scale);
 
     // Create map container
     var svg = d3.select('.map-container')
@@ -38,7 +39,7 @@ function drawMap(map) {
         .data(map.features)
         .enter()
         .append('path')
-        .attr("class", "country")
+        .attr('class', 'country')
         .attr('d', path);
 
 }
@@ -68,20 +69,18 @@ function displayTowns() {
                 .attr('r', d => calculateRadius(d.Population))
                 .attr('class', 'point')
                 .on('mouseover', function (event, d) {
-
                     d3.select(this).classed('hover', true);
                     updateToolTip(d);
-
                 })
-                .on("mouseout", function () {
+                .on('mouseout', function () {
                     d3.select(this).classed('hover', false);
                     tooltip.style('opacity', 0);
                 })
 
             c.transition()
                 .duration(1000)
-                .attr('cx', function (d) { return projection([d.lng, d.lat])[0]; })
-                .attr('cy', function (d) { return projection([d.lng, d.lat])[1]; })
+                .attr('cx', function (d) { return getXY(d)[0] })
+                .attr('cy', function (d) { return getXY(d)[1] })
 
             // Plot points town name
             svg.selectAll('.point-label')
@@ -89,8 +88,8 @@ function displayTowns() {
                 .enter()
                 .append('text')
                 .attr('class', 'point-label')
-                .attr('x', function (d) { return projection([d.lng, d.lat])[0] + 10 })
-                .attr('y', function (d) { return projection([d.lng, d.lat])[1] })
+                .attr('x', function (d) { return getXY(d)[0] + 10 })
+                .attr('y', function (d) { return getXY(d)[1] })
                 .text(function (d) { return d.Town; })
         });
 }
@@ -103,16 +102,20 @@ function clearData() {
 }
 
 function calculateRadius(population) {
-    return Math.max((population / 12000), 5);
+    return Math.max((population / 12000), 8);
 }
 
 function updateToolTip(d) {
     var tooltip = d3.select('.tool-tip')
 
     tooltip.html("<p>Town Name: " + d.Town + "</p><p>Population: " + d.Population + "</p>")
-        .style('left', projection([d.lng, d.lat])[0] + 50 + 'px')
-        .style('top', projection([d.lng, d.lat])[1] + 'px')
+        .style('left', getXY(d)[0] + 50 + 'px')
+        .style('top', getXY(d)[1] + 'px')
         .style('opacity', 1);
+}
+
+function getXY(d){
+    return projection([d.lng, d.lat]);
 }
 
 window.onload = function () {
